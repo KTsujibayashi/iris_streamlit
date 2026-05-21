ec084cafbac81b9ffa1300e7173648b68dcdf1a4
辻林宏典 <chankoro0316@gmail.com>
2026-05-21 21:52:52 +0900

    3nd commit

--- iris.py
+++ iris.py
@@ -45,7 +45,7 @@ st.write('## Input Value')
 # インプットデータ（1行のデータフレーム）
 value_df = pd.DataFrame([], columns=['data', 'special length (cm)', 'petal length (cm)'])
 record = pd.Series(['data', sepalValue, petalValue], index=value_df.columns)
-value_df = value_df.append(record, ignore_index=True)
+value_df = value_df.concat([value_df, pd.DataFrame([record])], ignore_index=True)
 value_df.set_index('data', inplace=True)
 
 # 入力値の値
